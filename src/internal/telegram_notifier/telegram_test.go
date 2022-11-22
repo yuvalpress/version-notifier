ffc87cb0b8f1504c16da0e51e584e36677ceb962
yuvalpress <yuvalpress@gmail.com>
2022-11-22 22:59:48 +0200

    remove token

--- src/internal/telegram_notifier/telegram_test.go
+++ src/internal/telegram_notifier/telegram_test.go
@@ -1,15 +1,10 @@
 package telegram_notifier
 
 import (
-	"os"
 	"testing"
 )
 
 func TestNotify(t *testing.T) {
-	// TODO: Fill this values using GitHub Actions
-	_ = os.Setenv("TELEGRAM_TOKEN", "")
-	_ = os.Setenv("TELEGRAM_CHAT_ID", "")
-
 	Notify("google",
 		"go-github",
 		"https://github.com/google/go-github/releases/tag/v48.1.0",
