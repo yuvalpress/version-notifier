c5ae78f4006fafa10ee0c488f82576cbad2548a3
yuvalpress <yuvalpress@gmail.com>
2022-12-23 19:23:50 +0200

    [skip all]

--- src/internal/slack_notifier/slack_test.go
+++ src/internal/slack_notifier/slack_test.go
@@ -8,12 +8,12 @@ func TestTelegramNotify(t *testing.T) {
 	Notify("google",
 		"go-github",
 		"https://github.com/google/go-github/releases/tag/v48.1.0",
-		"v48.0.0", "v48.1.0", "minor", false)
+		"v48.0.0", "v48.1.0", "minor", "release", false)
 }
 
 func TestTelegramNotifyWithRelease(t *testing.T) {
 	Notify("google",
 		"go-github",
 		"https://github.com/google/go-github/releases/tag/v48.1.0",
-		"v48.0.0", "v48.1.0", "minor", true)
+		"v48.0.0", "v48.1.0", "minor", "release", true)
 }
